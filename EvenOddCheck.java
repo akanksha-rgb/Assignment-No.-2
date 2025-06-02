052303dcadbf192c8fa6f8831c4240974b8b24a0
akanksha-rgb <akankshavl2001@gmail.com>
2025-06-02 11:57:06 +0530

    Update EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -5,12 +5,8 @@ import java.util.Scanner;
 public class EvenOddCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter a number
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
-
-        // Check if the number is even or odd
         if (number % 2 == 0) {
             System.out.println(number + " is even.");
         } else {
