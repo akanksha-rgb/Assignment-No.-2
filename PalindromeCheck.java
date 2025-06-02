aa36e97c6e869e1300389def4e012fd403f56aae
akanksha-rgb <akankshavl2001@gmail.com>
2025-06-02 11:58:01 +0530

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -5,15 +5,11 @@ public class PalindromeCheck {
         int number = 121;
         int originalNumber = number;
         int reverse = 0;
-
-        // Reverse the number
         while (number != 0) {
             int digit = number % 10;
             reverse = reverse * 10 + digit;
             number = number / 10;
         }
-
-        // Check if original and reversed numbers are the same
         if (originalNumber == reverse) {
             System.out.println(originalNumber + " is a palindrome.");
         } else {
